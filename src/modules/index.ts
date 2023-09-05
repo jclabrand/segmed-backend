0dac9bf5fa5ee1d247067569f3c55b4ce0929d61
Juan Carlos <jcharlylc@gmail.com>
2023-09-05 13:28:51 -0400

    Resolvers

---
+++ src/modules/index.ts
@@ -0,0 +1,10 @@
+
+/*	Copyright (C) 2023, All Rights Reserved
+ *	Written by Juan Carlos Labrandero <jcharly@labrandero.com>
+ */
+
+import Resolver from "./resolver"
+
+export {
+	Resolver
+}
---
+++ src/modules/types.ts
@@ -0,0 +1,14 @@
+
+/*	Copyright (C) 2023, All Rights Reserved
+ *	Written by Juan Carlos Labrandero <jcharly@labrandero.com>
+ */
+
+interface IContextValue {
+	auth?: String
+}
+
+interface IUser {
+	id: String
+}
+
+export { IUser, IContextValue }
