9bc229fb8ee92b18df3f096e737561040c206994
Juan Carlos <jcharlylc@gmail.com>
2023-09-19 12:47:23 -0400

    Rosolvers for authorizations

--- src/main/resolver.ts
+++ src/main/resolver.ts
@@ -8,20 +8,43 @@ import path from 'path'
 import { makeExecutableSchema } from '@graphql-tools/schema'
 import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge'
 import { loadFilesSync } from '@graphql-tools/load-files'
-import { IResolvers } from '@graphql-tools/utils'
+import { IResolvers, mapSchema, MapperKind, getDirective } from '@graphql-tools/utils'
 import { GraphQLSchema, DocumentNode } from 'graphql'
+import { PubSub } from 'graphql-subscriptions'
 
+import AuthResolver from '../modules/account/auth/auth-resolver'
 import UserResolver from '../modules/account/user/user-resolver'
-import { IContextValue } from './types'
+import { IContextValue, SubscriptionEvents } from './types'
 
 class Resolver {
 
-	schema : GraphQLSchema
+	schema:	GraphQLSchema
+	pubsub:	PubSub
 
 	constructor() {
-		this.schema = makeExecutableSchema({
+		this.pubsub = new PubSub()
+
+		let basicSchema = makeExecutableSchema({
 			typeDefs: this.buildTypeDefs(),
 			resolvers: this.buildResolvers()
+		})
+
+		this.schema = this.buildDirectives(basicSchema)
+	}
+
+	buildDirectives(schema: GraphQLSchema): GraphQLSchema {
+		const auth = new AuthResolver()
+
+		return mapSchema(schema, {
+			[MapperKind.OBJECT_FIELD]: fieldConfig => {
+				const authDirective = getDirective(schema, fieldConfig, 'authorized')
+
+				if (authDirective) {
+					fieldConfig.resolve = auth.authorized(fieldConfig.resolve)
+				}
+
+				return fieldConfig
+			}
 		})
 	}
 
@@ -31,23 +54,32 @@ class Resolver {
 	}
 
 	buildResolvers<TSource>(): IResolvers<TSource, IContextValue> {
-		const user = new UserResolver()
+		const pubsub = this.pubsub
+			, user = new UserResolver()
 
 		return mergeResolvers([
 			{
 				Query: {
-					users: user.index
+					users: user.index,
+					currentUser: user.current
 				},
 				Mutation: {
+					signIn: user.signIn,
+					signOut: user.signOut,
 					createUser: user.create
+				},
+				Subscription: {
+					userAdded: {
+						subscribe: () => pubsub.asyncIterator(SubscriptionEvents.USER_ADDED)
+					}
 				}
 			}
 		])
 	}
 
-	buildContext(context: IContextValue) {
-		return async (): Promise<IContextValue> => {
-			return context
+	buildContext(db, pubsub) {
+		return async ({ req }): Promise<IContextValue> => {
+			return { req, db, pubsub }
 		}
 	}
 }
